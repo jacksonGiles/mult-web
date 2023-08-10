77c9bce7fe7adc900dd29de7d74ce5667225854a
Jackson Giles <jackson@smallsky.dev>
2023-08-10 14:36:18 -0400

    music page init

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <img class="music-icon" src="../assets/smudgebackground.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  components: {},
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.music-icon {
+  height: 10px;
+}
+</style>
