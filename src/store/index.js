80e57224e7261508316a2bc7944dd50a4b31e083
Jackson Giles <jackson@smallsky.dev>
2023-11-26 13:30:58 -0500

    transition state added for animations

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,25 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     menuOpen: false,
+    isTransitioning: false,
   },
-  getters: {},
   mutations: {
     setMenuOpen(state, value) {
       state.menuOpen = value;
+    },
+    setIsTransitioning(state, value) {
+      state.isTransitioning = value;
     },
   },
   actions: {
     toggleMenu({ commit, state }) {
       commit("setMenuOpen", !state.menuOpen);
+    },
+    startTransition({ commit }) {
+      commit("setIsTransitioning", true);
+    },
+    endTransition({ commit }) {
+      commit("setIsTransitioning", false);
     },
   },
   modules: {},
