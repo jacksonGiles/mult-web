2c08d119a431d7cc7f407c09f76ce3b0a4effb19
Jackson Giles <jackson@smallsky.dev>
2023-11-26 13:40:07 -0500

    styles

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { createVuetify } from "vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import "/styles.scss";
 
 loadFonts();
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+});
