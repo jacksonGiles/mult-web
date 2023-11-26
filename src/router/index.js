28247b35a76bba06d0d3276932acc79204eb93b7
Jackson Giles <jackson@smallsky.dev>
2023-11-26 13:31:40 -0500

    added Contact, Listen, and Sandbox routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import ContactView from "../views/ContactView.vue";
 import MusicView from "../views/MusicView.vue";
 import MerchView from "../views/MerchView.vue";
+import ListenView from "../views/ListenView.vue";
+import SandboxView from "../views/SandboxView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
   },
   {
     path: "/about",
-    name: "about",
+    name: "About",
     component: AboutView,
   },
   {
+    path: "/contact",
+    name: "Contact",
+    component: ContactView,
+  },
+  {
+    path: "/listen",
+    name: "Listen",
+    component: ListenView,
+  },
+  {
     path: "/music",
-    name: "music",
+    name: "Music",
     component: MusicView,
   },
   {
     path: "/merch",
-    name: "merch",
+    name: "Merch",
     component: MerchView,
+  },
+  {
+    path: "/sandbox",
+    name: "Sandbox",
+    component: SandboxView,
   },
 ];
 
