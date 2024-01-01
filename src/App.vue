0ae4ac928819f5c7884d449636665ea4ecc97406
Jackson Giles <jackson@smallsky.dev>
2024-01-01 04:27:17 -0500

    animations and load-in

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div
-    class="mult-page"
-    :class="listen ? 'listen' : ''"
-    :style="
-      listen
-        ? {
-            'background-image': 'url(' + require('./assets/SMUDGE4.png') + ')',
-            'background-size': '100%',
-            'background-position': bgAltPosition,
-          }
-        : {
-            'background-image':
-              'url(' + require('./assets/smudgebackground.png') + ')',
-            'background-size': '100%',
-            'background-position': bgPosition,
-          }
-    "
+    :class="listen ? 'listen mult-page' : 'mult-page'"
+    :style="getBackgroundStyle"
     @mousemove="updateBgPosition"
   >
     <MultHeader :listen="listen" />
@@ -29,30 +15,81 @@
     </transition>
     <router-view @listen="listenPage" />
   </div>
+  <LoadIn
+    v-if="!imagesLoaded"
+    class="load-in"
+    style="
+      z-index: 9999999;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    "
+  />
+  <!-- <LoadIn v-if="!imagesLoaded" class="load-in" /> -->
 </template>
 
 <script>
 import "@mdi/font/css/materialdesignicons.css";
 import MultHeader from "./components/MultHeader.vue";
 import MenuOverlay from "./components/MenuOverlay.vue";
+import LoadIn from "./components/LoadIn.vue";
 import { gsap } from "gsap";
 export default {
   components: {
     MultHeader,
     MenuOverlay,
+    LoadIn,
   },
-  mounted() {
+  async mounted() {
     this.$nextTick(() => {
-      // Calculate initial position based on center of screen
       this.updateBgPosition({
         clientX: window.innerWidth / 2,
         clientY: window.innerHeight / 2,
       });
     });
+
+    setTimeout(async () => {
+      await this.imagePreload();
+      if (this.imagesLoaded) {
+        gsap.from(".mult-page", {
+          duration: 2,
+          opacity: 0,
+          ease: "power2.out",
+        });
+      }
+    }, 1000);
   },
   computed: {
     menuOpen() {
       return this.$store.state.menuOpen;
+    },
+    getBackgroundStyle() {
+      let backgroundSize, backgroundPosition;
+
+      if (this.listen) {
+        backgroundSize = this.$vuetify.display.smAndDown ? "180%" : "100%";
+        backgroundPosition = this.$vuetify.display.smAndDown
+          ? this.mobileAltPosition
+          : this.bgAltPosition;
+      } else {
+        backgroundSize = this.$vuetify.display.smAndDown
+          ? "200%"
+          : this.$vuetify.display.mdAndDown
+          ? "108%"
+          : "100%";
+        backgroundPosition = this.$vuetify.display.smAndDown
+          ? this.mobilePosition
+          : this.bgPosition;
+      }
+
+      return {
+        "background-image": `url(${
+          this.listen ? this.smudgeAlt : this.smudge
+        })`,
+        "background-size": backgroundSize,
+        "background-position": backgroundPosition,
+      };
     },
   },
   data() {
@@ -60,8 +97,15 @@ export default {
       image: "./assets/smudgebackground.png",
       // expandMenu: false,
       bgPosition: "50% 100%",
+      mobilePosition: "70% 100%",
+
       bgAltPosition: "50% 50%",
+      mobileAltPosition: "70% 100%",
       listen: false,
+
+      imagesLoaded: false,
+      smudgeAlt: null,
+      smudge: null,
     };
   },
   methods: {
@@ -74,8 +118,31 @@ export default {
     enterView(el, done) {
       gsap.to(el, { opacity: 1, duration: 0.4, onComplete: done });
     },
+    imagePreload() {
+      const images = [
+        require("./assets/SMUDGE4.png"),
+        require("./assets/smudgebackground.png"),
+      ];
+      const promises = images.map((imageSrc, index) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.onload = () => {
+            this[index === 0 ? "smudgeAlt" : "smudge"] = imageSrc;
+            resolve();
+          };
+          img.src = imageSrc;
+        });
+      });
+      setTimeout(() => {
+        this.imagesLoaded = true;
+      }, 3000);
+      return Promise.all(promises);
+    },
     leave(el, done) {
       gsap.to(el, { opacity: 0, duration: 0.4, onComplete: done });
+    },
+    listenPage(listen) {
+      this.listen = listen;
     },
     updateBgPosition(event) {
       let xPercent = (event.clientX / window.innerWidth - 0.5) * 3;
@@ -83,9 +150,6 @@ export default {
 
       this.bgPosition = `${50 + xPercent / 2}% ${50 + yPercent / 2}%`;
       this.bgAltPosition = `${50 + xPercent / 2}% ${50 + yPercent / 2}%`;
-    },
-    listenPage(listen) {
-      this.listen = listen;
     },
   },
 };
@@ -109,6 +173,9 @@ export default {
   text-align: center;
   color: #b8ddfa;
 }
+.load-in {
+  position: absolute;
+}
 .mult-page {
   width: 100vw;
   height: 100vh;
@@ -122,7 +189,7 @@ export default {
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity 2s;
 }
 .fade-enter,
 .fade-leave-to {
---
+++ src/components/LoadIn.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="load-container">
+    <img class="load" :src="require('../assets/multlogo2.png')" alt="" />
+  </div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+
+export default {
+  async mounted() {
+    this.animate();
+  },
+  methods: {
+    animate() {
+      var tl = gsap.timeline();
+
+      tl.from(".load", {
+        duration: 2,
+        opacity: 0,
+        ease: "power2.out",
+      });
+
+      tl.to(".load-container", {
+        duration: 1,
+        opacity: 0,
+        ease: "power2.out",
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.load-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #a7cae4;
+}
+.load {
+  height: 5vh;
+}
+</style>
