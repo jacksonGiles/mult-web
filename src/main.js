6561938e2e931f5a6e7845d2866465d0fe25b6dc
Jackson Giles <jackson@smallsky.dev>
2024-01-01 04:22:17 -0500

    main additions

--- src/main.js
+++ src/main.js
@@ -2,17 +2,27 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
-import { createVuetify } from "vuetify";
+import { createVuetify } from "vuetify"; // Import createVuetify
+import "vuetify/styles"; // Import Vuetify styles
 import { loadFonts } from "./plugins/webfontloader";
 import "/styles.scss";
+import breakpointMixin from "./mixins/breakpointMixin";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
-
-export default createVuetify({
+// Create a Vuetify instance
+const vuetify = createVuetify({
   theme: {
     defaultTheme: "dark",
   },
 });
+
+// Create the Vue application
+const app = createApp(App);
+
+app
+  .use(router)
+  .use(store)
+  .use(vuetify) // Use the Vuetify instance
+  .mount("#app");
+app.mixin(breakpointMixin);
