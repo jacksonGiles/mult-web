5d6f10960a5537867833d7bb37b730c1de8f7e39
Jackson Giles <jackson@smallsky.dev>
2024-04-19 12:47:46 -0400

    updated vuex store to set time values and pause/play values

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,13 @@ export default createStore({
   state: {
     menuOpen: false,
     isTransitioning: false,
+    currentTime: "0:00",
+    duration: "0:00",
+    play: false,
+    title: "---",
+    current: 0,
+    total: 0,
+    progress: 0,
   },
   mutations: {
     setMenuOpen(state, value) {
@@ -11,6 +18,25 @@ export default createStore({
     },
     setIsTransitioning(state, value) {
       state.isTransitioning = value;
+    },
+    setCurrentTime(state, currentTime) {
+      state.currentTime = currentTime;
+    },
+    setDuration(state, duration) {
+      state.duration = duration;
+    },
+    togglePlay(state, value) {
+      state.play = value;
+    },
+    setTitle(state, title) {
+      state.title = title;
+    },
+    setProgress(state, { current, total }) {
+      state.current = current;
+      state.total = total;
+    },
+    setCurrent(state, current) {
+      state.current = current;
     },
   },
   actions: {
@@ -22,6 +48,24 @@ export default createStore({
     },
     endTransition({ commit }) {
       commit("setIsTransitioning", false);
+    },
+    setCurrentTime({ commit }, currentTime) {
+      commit("setCurrentTime", currentTime);
+    },
+    setDuration({ commit }, duration) {
+      commit("setDuration", duration);
+    },
+    togglePlay({ commit, state }) {
+      commit("togglePlay", !state.play);
+    },
+    setTitle({ commit }, title) {
+      commit("setTitle", title);
+    },
+    setCurrent({ commit }, current) {
+      commit("setCurrent", current);
+    },
+    setProgress({ commit }, { current, total }) {
+      commit("setProgress", { current, total });
     },
   },
   modules: {},
